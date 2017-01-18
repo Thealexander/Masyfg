21a82c7771f032b52051fef2f2bbd7a0cca7e028
Brayan <brayan.gaitan@uam.net.ni>
2017-01-18 10:29:15 -0600

    Actualizacion de Documentacion
    Documentacion

---
+++ Documentacion/7. Prototipos (Mockups)/2. Generados/Mockups del sistema_0/resources/screens/33b5b69f-a8ce-416e-a7d1-752bcfebb470-1479316012830.js
@@ -0,0 +1,295 @@
+jQuery("#simulation")
+  .on("click", ".s-33b5b69f-a8ce-416e-a7d1-752bcfebb470 .click", function(event, data) {
+    var jEvent, jFirer, cases;
+    if(data === undefined) { data = event; }
+    jEvent = jimEvent(event);
+    jFirer = jEvent.getEventFirer();
+    if(jFirer.is("#s-Menu_item_10")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/84306bee-7d62-420c-a5bb-105ee2183b75"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Menu_item_11")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/53fce2b3-d0fc-4159-be33-5f3a415a0924"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Menu_item_12")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/c4b25d68-99db-47a6-beb2-c9e57be9cf1d"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Menu_item_13")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/382ca640-0de4-4201-9667-e38d9d75483e"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Menu_item_14")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/912da97e-37d1-457b-a315-1e24b59f31c3"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Menu_item_15")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/1ec5e3f3-ebf0-41c1-b0ac-1754effc5e0d"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Menu_item_17")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/032d8f3a-19d2-464a-b27f-d2e5663edd99"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Menu_item_18")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/f86434c1-bc31-48b9-9722-f8c992b69e16"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Menu_item_19")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/4806e588-82b0-4165-9b40-79ab63e3e8c2"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    }
+  })
+  .on("pageload", ".s-33b5b69f-a8ce-416e-a7d1-752bcfebb470 .pageload", function(event, data) {
+    var jEvent, jFirer, cases;
+    if(data === undefined) { data = event; }
+    jEvent = jimEvent(event);
+    jFirer = jEvent.getEventFirer();
+    if(jFirer.is("#s-Panel_7")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimMove",
+                  "parameter": {
+                    "target": "#s-Pinned-top",
+                    "top": {
+                      "type": "exprvalue",
+                      "value": {
+                        "action": "jimWindowScrollY"
+                      }
+                    },
+                    "left": {
+                      "type": "nomove"
+                    },
+                    "containment": false
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    }
+  })
+  .on("windowscroll", ".s-33b5b69f-a8ce-416e-a7d1-752bcfebb470 .windowscroll", function(event, data) {
+    var jEvent, jFirer, cases;
+    if(data === undefined) { data = event; }
+    jEvent = jimEvent(event);
+    jFirer = jEvent.getEventFirer();
+    if(jFirer.is("#s-Panel_7")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimMove",
+                  "parameter": {
+                    "target": "#s-Pinned-top",
+                    "top": {
+                      "type": "exprvalue",
+                      "value": {
+                        "action": "jimWindowScrollY"
+                      }
+                    },
+                    "left": {
+                      "type": "nomove"
+                    },
+                    "containment": false
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    }
+  });
---
+++ Documentacion/7. Prototipos (Mockups)/2. Generados/Mockups del sistema_0/resources/simulationdata-1479316012830.js
@@ -0,0 +1,541 @@
+function initData() {
+  jimData.variables["Estudiantes"] = "";
+  jimData.datamasters["Colegios"] = [
+    {
+      "id": 1,
+      "datamaster": "Colegios",
+      "userdata": {
+        "Colegio": "sample text",
+        "Proyecto": "sample text",
+        "Director": "sample text"
+      }
+    },
+    {
+      "id": 2,
+      "datamaster": "Colegios",
+      "userdata": {
+        "Colegio": "sample text",
+        "Proyecto": "sample text",
+        "Director": "sample text"
+      }
+    },
+    {
+      "id": 3,
+      "datamaster": "Colegios",
+      "userdata": {
+        "Colegio": "sample text",
+        "Proyecto": "sample text",
+        "Director": "sample text"
+      }
+    },
+    {
+      "id": 4,
+      "datamaster": "Colegios",
+      "userdata": {
+        "Colegio": "sample text",
+        "Proyecto": "sample text",
+        "Director": "sample text"
+      }
+    },
+    {
+      "id": 5,
+      "datamaster": "Colegios",
+      "userdata": {
+        "Colegio": "sample text",
+        "Proyecto": "sample text",
+        "Director": "sample text"
+      }
+    },
+    {
+      "id": 6,
+      "datamaster": "Colegios",
+      "userdata": {
+        "Colegio": "sample text",
+        "Proyecto": "sample text",
+        "Director": "sample text"
+      }
+    }
+  ];
+
+  jimData.datamasters["Proyectos"] = [
+    {
+      "id": 1,
+      "datamaster": "Proyectos",
+      "userdata": {
+        "IDProyecto": "./resources/_jim/images/common/crossimage.png",
+        "Proyecto": "sample text",
+        "Empresa Socia": "sample text",
+        "Coordinador": "sample text"
+      }
+    },
+    {
+      "id": 2,
+      "datamaster": "Proyectos",
+      "userdata": {
+        "IDProyecto": "./resources/_jim/images/common/crossimage.png",
+        "Proyecto": "sample text",
+        "Empresa Socia": "sample text",
+        "Coordinador": "sample text"
+      }
+    },
+    {
+      "id": 3,
+      "datamaster": "Proyectos",
+      "userdata": {
+        "IDProyecto": "./resources/_jim/images/common/crossimage.png",
+        "Proyecto": "sample text",
+        "Empresa Socia": "sample text",
+        "Coordinador": "sample text"
+      }
+    },
+    {
+      "id": 4,
+      "datamaster": "Proyectos",
+      "userdata": {
+        "IDProyecto": "./resources/_jim/images/common/crossimage.png",
+        "Proyecto": "sample text",
+        "Empresa Socia": "sample text",
+        "Coordinador": "sample text"
+      }
+    },
+    {
+      "id": 5,
+      "datamaster": "Proyectos",
+      "userdata": {
+        "IDProyecto": "./resources/_jim/images/common/crossimage.png",
+        "Proyecto": "sample text",
+        "Empresa Socia": "sample text",
+        "Coordinador": "sample text"
+      }
+    },
+    {
+      "id": 6,
+      "datamaster": "Proyectos",
+      "userdata": {
+        "IDProyecto": "./resources/_jim/images/common/crossimage.png",
+        "Proyecto": "sample text",
+        "Empresa Socia": "sample text",
+        "Coordinador": "sample text"
+      }
+    }
+  ];
+
+  jimData.datamasters["Profesores"] = [
+    {
+      "id": 1,
+      "datamaster": "Profesores",
+      "userdata": {
+        "Apellidos": "sample text",
+        "Nombres": "sample text",
+        "Colegio": "sample text"
+      }
+    },
+    {
+      "id": 2,
+      "datamaster": "Profesores",
+      "userdata": {
+        "Apellidos": "sample text",
+        "Nombres": "sample text",
+        "Colegio": "sample text"
+      }
+    },
+    {
+      "id": 3,
+      "datamaster": "Profesores",
+      "userdata": {
+        "Apellidos": "sample text",
+        "Nombres": "sample text",
+        "Colegio": "sample text"
+      }
+    },
+    {
+      "id": 4,
+      "datamaster": "Profesores",
+      "userdata": {
+        "Apellidos": "sample text",
+        "Nombres": "sample text",
+        "Colegio": "sample text"
+      }
+    },
+    {
+      "id": 5,
+      "datamaster": "Profesores",
+      "userdata": {
+        "Apellidos": "sample text",
+        "Nombres": "sample text",
+        "Colegio": "sample text"
+      }
+    },
+    {
+      "id": 6,
+      "datamaster": "Profesores",
+      "userdata": {
+        "Apellidos": "sample text",
+        "Nombres": "sample text",
+        "Colegio": "sample text"
+      }
+    }
+  ];
+
+  jimData.datamasters["Estudiantes"] = [
+    {
+      "id": 1,
+      "datamaster": "Estudiantes",
+      "userdata": {
+        "Apellidos": "sample text",
+        "Nombres": "sample text",
+        "Colegio": "sample text",
+        "Club": "sample text"
+      }
+    },
+    {
+      "id": 2,
+      "datamaster": "Estudiantes",
+      "userdata": {
+        "Apellidos": "sample text",
+        "Nombres": "sample text",
+        "Colegio": "sample text",
+        "Club": "sample text"
+      }
+    },
+    {
+      "id": 3,
+      "datamaster": "Estudiantes",
+      "userdata": {
+        "Apellidos": "sample text",
+        "Nombres": "sample text",
+        "Colegio": "sample text",
+        "Club": "sample text"
+      }
+    },
+    {
+      "id": 4,
+      "datamaster": "Estudiantes",
+      "userdata": {
+        "Apellidos": "sample text",
+        "Nombres": "sample text",
+        "Colegio": "sample text",
+        "Club": "sample text"
+      }
+    },
+    {
+      "id": 5,
+      "datamaster": "Estudiantes",
+      "userdata": {
+        "Apellidos": "sample text",
+        "Nombres": "sample text",
+        "Colegio": "sample text",
+        "Club": "sample text"
+      }
+    },
+    {
+      "id": 6,
+      "datamaster": "Estudiantes",
+      "userdata": {
+        "Apellidos": "sample text",
+        "Nombres": "sample text",
+        "Colegio": "sample text",
+        "Club": "sample text"
+      }
+    }
+  ];
+
+  jimData.datamasters["EmpresasSocias"] = [
+    {
+      "id": 1,
+      "datamaster": "EmpresasSocias",
+      "userdata": {
+        "Nombre de la Empresa": "sample text",
+        "Contacto": "sample text",
+        "Telefono": "sample text"
+      }
+    },
+    {
+      "id": 2,
+      "datamaster": "EmpresasSocias",
+      "userdata": {
+        "Nombre de la Empresa": "sample text",
+        "Contacto": "sample text",
+        "Telefono": "sample text"
+      }
+    },
+    {
+      "id": 3,
+      "datamaster": "EmpresasSocias",
+      "userdata": {
+        "Nombre de la Empresa": "sample text",
+        "Contacto": "sample text",
+        "Telefono": "sample text"
+      }
+    },
+    {
+      "id": 4,
+      "datamaster": "EmpresasSocias",
+      "userdata": {
+        "Nombre de la Empresa": "sample text",
+        "Contacto": "sample text",
+        "Telefono": "sample text"
+      }
+    },
+    {
+      "id": 5,
+      "datamaster": "EmpresasSocias",
+      "userdata": {
+        "Nombre de la Empresa": "sample text",
+        "Contacto": "sample text",
+        "Telefono": "sample text"
+      }
+    },
+    {
+      "id": 6,
+      "datamaster": "EmpresasSocias",
+      "userdata": {
+        "Nombre de la Empresa": "sample text",
+        "Contacto": "sample text",
+        "Telefono": "sample text"
+      }
+    }
+  ];
+
+  jimData.datamasters["Clubs"] = [
+    {
+      "id": 1,
+      "datamaster": "Clubs",
+      "userdata": {
+        "IdClub": "sample text",
+        "Club": "sample text",
+        "Proyecto": "sample text"
+      }
+    },
+    {
+      "id": 2,
+      "datamaster": "Clubs",
+      "userdata": {
+        "IdClub": "sample text",
+        "Club": "sample text",
+        "Proyecto": "sample text"
+      }
+    },
+    {
+      "id": 3,
+      "datamaster": "Clubs",
+      "userdata": {
+        "IdClub": "sample text",
+        "Club": "sample text",
+        "Proyecto": "sample text"
+      }
+    },
+    {
+      "id": 4,
+      "datamaster": "Clubs",
+      "userdata": {
+        "IdClub": "sample text",
+        "Club": "sample text",
+        "Proyecto": "sample text"
+      }
+    },
+    {
+      "id": 5,
+      "datamaster": "Clubs",
+      "userdata": {
+        "IdClub": "sample text",
+        "Club": "sample text",
+        "Proyecto": "sample text"
+      }
+    },
+    {
+      "id": 6,
+      "datamaster": "Clubs",
+      "userdata": {
+        "IdClub": "sample text",
+        "Club": "sample text",
+        "Proyecto": "sample text"
+      }
+    }
+  ];
+
+  jimData.datamasters["Colaboradores"] = [
+    {
+      "id": 1,
+      "datamaster": "Colaboradores",
+      "userdata": {
+        "Id": "sample text",
+        "Apellidos Colaborador": "sample text",
+        "Nombres Colaborador": "sample text",
+        "Cargo": "sample text"
+      }
+    },
+    {
+      "id": 2,
+      "datamaster": "Colaboradores",
+      "userdata": {
+        "Id": "sample text",
+        "Apellidos Colaborador": "sample text",
+        "Nombres Colaborador": "sample text",
+        "Cargo": "sample text"
+      }
+    },
+    {
+      "id": 3,
+      "datamaster": "Colaboradores",
+      "userdata": {
+        "Id": "sample text",
+        "Apellidos Colaborador": "sample text",
+        "Nombres Colaborador": "sample text",
+        "Cargo": "sample text"
+      }
+    },
+    {
+      "id": 4,
+      "datamaster": "Colaboradores",
+      "userdata": {
+        "Id": "sample text",
+        "Apellidos Colaborador": "sample text",
+        "Nombres Colaborador": "sample text",
+        "Cargo": "sample text"
+      }
+    },
+    {
+      "id": 5,
+      "datamaster": "Colaboradores",
+      "userdata": {
+        "Id": "sample text",
+        "Apellidos Colaborador": "sample text",
+        "Nombres Colaborador": "sample text",
+        "Cargo": "sample text"
+      }
+    },
+    {
+      "id": 6,
+      "datamaster": "Colaboradores",
+      "userdata": {
+        "Id": "sample text",
+        "Apellidos Colaborador": "sample text",
+        "Nombres Colaborador": "sample text",
+        "Cargo": "sample text"
+      }
+    }
+  ];
+
+  jimData.datamasters["Pasantes"] = [
+    {
+      "id": 1,
+      "datamaster": "Pasantes",
+      "userdata": {
+        "ID": "sample text",
+        "Apellidos": "sample text",
+        "Nombres": "sample text",
+        "Universidad": "sample text"
+      }
+    },
+    {
+      "id": 2,
+      "datamaster": "Pasantes",
+      "userdata": {
+        "ID": "sample text",
+        "Apellidos": "sample text",
+        "Nombres": "sample text",
+        "Universidad": "sample text"
+      }
+    },
+    {
+      "id": 3,
+      "datamaster": "Pasantes",
+      "userdata": {
+        "ID": "sample text",
+        "Apellidos": "sample text",
+        "Nombres": "sample text",
+        "Universidad": "sample text"
+      }
+    },
+    {
+      "id": 4,
+      "datamaster": "Pasantes",
+      "userdata": {
+        "ID": "sample text",
+        "Apellidos": "sample text",
+        "Nombres": "sample text",
+        "Universidad": "sample text"
+      }
+    },
+    {
+      "id": 5,
+      "datamaster": "Pasantes",
+      "userdata": {
+        "ID": "sample text",
+        "Apellidos": "sample text",
+        "Nombres": "sample text",
+        "Universidad": "sample text"
+      }
+    },
+    {
+      "id": 6,
+      "datamaster": "Pasantes",
+      "userdata": {
+        "ID": "sample text",
+        "Apellidos": "sample text",
+        "Nombres": "sample text",
+        "Universidad": "sample text"
+      }
+    }
+  ];
+
+  jimData.datamasters["Voluntarios"] = [
+    {
+      "id": 1,
+      "datamaster": "Voluntarios",
+      "userdata": {
+        "Apellidos": "sample text",
+        "Nombres": "sample text",
+        "Organizacion de Origen": "sample text"
+      }
+    },
+    {
+      "id": 2,
+      "datamaster": "Voluntarios",
+      "userdata": {
+        "Apellidos": "sample text",
+        "Nombres": "sample text",
+        "Organizacion de Origen": "sample text"
+      }
+    },
+    {
+      "id": 3,
+      "datamaster": "Voluntarios",
+      "userdata": {
+        "Apellidos": "sample text",
+        "Nombres": "sample text",
+        "Organizacion de Origen": "sample text"
+      }
+    },
+    {
+      "id": 4,
+      "datamaster": "Voluntarios",
+      "userdata": {
+        "Apellidos": "sample text",
+        "Nombres": "sample text",
+        "Organizacion de Origen": "sample text"
+      }
+    },
+    {
+      "id": 5,
+      "datamaster": "Voluntarios",
+      "userdata": {
+        "Apellidos": "sample text",
+        "Nombres": "sample text",
+        "Organizacion de Origen": "sample text"
+      }
+    },
+    {
+      "id": 6,
+      "datamaster": "Voluntarios",
+      "userdata": {
+        "Apellidos": "sample text",
+        "Nombres": "sample text",
+        "Organizacion de Origen": "sample text"
+      }
+    }
+  ];
+
+  jimData.isInitialized = true;
+}
